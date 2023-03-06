00df742925e50a8d3e817d591b4e5137610443fd
expoure <dkramer97@hotmail.com>
2023-03-06 17:41:09 -0300

    slices ending

--- arrays-and-slices/sum/sum_test.go
+++ arrays-and-slices/sum/sum_test.go
@@ -25,3 +25,28 @@ func TestAllSum(t *testing.T) {
 		}
 	})
 }
+
+func TestSumAllTails(t *testing.T) {
+	t.Helper()
+	checkSums := func(t *testing.T, got, want []int) {
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d want %d given", got, want)
+		}
+	}
+
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+
+		checkSums(t, got, want)
+
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{0, 9})
+		want := []int{0, 9}
+
+		checkSums(t, got, want)
+
+	})
+}
