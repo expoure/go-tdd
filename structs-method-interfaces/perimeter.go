b220531504c633a8de01d98ae92103e66e2339ad
expoure <dkramer97@hotmail.com>
2023-03-07 20:20:05 -0300

    Starting structs

---
+++ structs-method-interfaces/perimeter.go
@@ -0,0 +1,14 @@
+package perimeter
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func Area(rectangle Rectangle) float64 {
+	return (rectangle.Width * rectangle.Height)
+}
